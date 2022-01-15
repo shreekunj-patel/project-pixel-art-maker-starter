36906b2bdc06ca5e8fbbce9bd4bfa111e70e8b26
Shreekunj Patel <patelshreekunj@gmail.com>
2022-01-15 14:15:51 +0530

    Update: designs.js - Adds make2DArray() function

--- designs.js
+++ designs.js
@@ -8,3 +8,22 @@ function makeGrid() {
 // Your code goes here!
 
 }
+
+/**
+ * makes 2d array from given height(raw) and width(columns)
+ * @param {number} height total number of rows
+ * @param {number} width total number of columns
+ * @returns 2d array
+ */
+function make2DArray(height, width) {
+    let arr2d = [];
+    for (let i = 0; i < height; i++) {
+        arr2d.push([]);
+        for (let j = 0; j < width; j++) {
+            arr2d[i].push(j);
+        }
+    }
+    console.log(arr2d);
+
+    return arr2d;
+}
